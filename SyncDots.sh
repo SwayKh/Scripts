66472be82c1f704daa17773eba6e71d64f020557
Sumit Khatri <khatrisumit712@gmail.com>
2023-04-22 21:14:09 +0530

    Updated Syncdots.sh

--- SyncDots.sh
+++ SyncDots.sh
@@ -5,7 +5,7 @@ echo "=========Updating Linux Notes in Obsidian========="
 echo " "
 
 # Put nala history into file and trim the output to different file and then remove the original
-nala history | cut -c 45- --complement | cut -c -5 --complement > $HOME/dotfiles/NalaHistory.md
+nala history | cut -c 45- --complement | cut -c -6 --complement > $HOME/dotfiles/NalaHistory.md
 
 # grep the zsh_history file for install keyword and put it in dotfiles/installed.md
 cut -b 1-15 --complement $HOME/.zsh_history | grep -e 'install' -e 'git clone' -e 'wget' > $HOME/dotfiles/Installed.md
@@ -18,27 +18,35 @@ awk -i inplace '!seen[$0]++' $HOME/dotfiles/Installed.md
 rsync -avP $HOME/dotfiles/Installed.md $HOME/Obsidian/Vault/Linux
 rsync -avP $HOME/dotfiles/NalaHistory.md $HOME/Obsidian/Vault/Linux
 
-echo " "
-echo "=========Updating .obsidian folder(configs)========="
-echo " "
-
-rsync -avP $HOME/Obsidian/Vault/.obsidian/ $HOME/dotfiles/.obsidian
 
 echo " "
 echo "=========Updating SyncDots.sh========="
 echo " "
 
-rsync -avP $HOME/SyncDots.sh $HOME/dotfiles
+rsync -avP $HOME/SyncDots.sh $HOME/Scripts
 
 
 echo " "
 echo "=========Updating git repository========="
 echo " "
 
-
 # Push the dotfiles folder
 commit=$(date)
 cd $HOME/dotfiles
+pwd
+git add .
+read -p "Enter a Commit: " temp
+if [ "$temp" != "" ]; then
+	commit=$temp
+fi
+echo $commit
+
+git commit -m "$commit"
+git push
+
+# Push the Scripts Folder
+cd $HOME/Scripts
+pwd
 git add .
 read -p "Enter a Commit: " temp
 if [ "$temp" != "" ]; then
