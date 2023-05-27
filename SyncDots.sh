e07032c3c683616dc362a0692964432549fdae2c
Sumit Khatri <khatrisumit712@gmail.com>
2023-05-27 16:35:26 +0530

    Updated Syndots to put the notes in the obsidian folder

--- SyncDots.sh
+++ SyncDots.sh
@@ -5,18 +5,19 @@ echo "=========Updating Linux Notes in Obsidian========="
 echo " "
 
 # Put nala history into file and trim the output to different file and then remove the original
-nala history | cut -c 45- --complement | cut -c -6 --complement > "$HOME"/dotfiles/NalaHistory.md
+nala history | cut -c 45- --complement | cut -c -6 --complement > "$HOME"/Obsidian/Vault/Linux/NalaHistory.md
 
 # grep the zsh_history file for install keyword and put it in dotfiles/installed.md
-cut -b 1-15 --complement "$HOME"/.zsh_history | grep -e 'install' -e 'git clone' -e 'wget' > "$HOME"/dotfiles/Installed.md
+cut -b 1-15 --complement "$HOME"/.zsh_history | grep -e 'install' -e 'git clone' -e 'wget' > "$HOME"/Obsidian/Vault/Linux/Installed.md
 
 #Clean up NalaHistory and Installed with AWK
-awk -i inplace '!seen[$0]++' "$HOME"/dotfiles/NalaHistory.md
-awk -i inplace '!seen[$0]++' "$HOME"/dotfiles/Installed.md
+awk -i inplace '!seen[$0]++' "$HOME"/Obsidian/Vault/Linux/NalaHistory.md
+awk -i inplace '!seen[$0]++' "$HOME"/Obsidian/Vault/Linux/Installed.md
 
-# Needs to be rsynced,  symlinks don't work
-rsync -avP "$HOME"/dotfiles/Installed.md "$HOME"/Obsidian/Vault/Linux
-rsync -avP "$HOME"/dotfiles/NalaHistory.md "$HOME"/Obsidian/Vault/Linux
+# No Need to put the Notes in the dotfiles directory, directly put it in the obsidian directory
+# # Needs to be rsynced,  symlinks don't work
+# rsync -avP "$HOME"/dotfiles/Installed.md "$HOME"/Obsidian/Vault/Linux
+# rsync -avP "$HOME"/dotfiles/NalaHistory.md "$HOME"/Obsidian/Vault/Linux
 
 
 echo " "
@@ -38,8 +39,8 @@ git commit -m "$commit"
 git push
 
 # Push the Scripts Folder
-
 commit=$(date)
+
 cd "$HOME"/Scripts || exit
 pwd
 git add .
