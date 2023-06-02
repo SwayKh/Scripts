411bae8c4a7e9544234483d7dfa3472d503d2dcd
Sumit Khatri <khatrisumit712@gmail.com>
2023-06-02 18:32:45 +0530

    Updated lfInstall script

--- testScript.sh
+++ testScript.sh
@@ -1,36 +1,4 @@
-#! /bin/bash
-
-# Set up LF file manager with one script, Installs all the packages and configs needed
-
-# Download the latest release from github
-echo "Downloading Latest Release"
-
-# Download the archive
-wget https://github.com/horriblename/lf/releases/latest/download/lf-linux-amd64.tar.gz > /dev/null 2>&1
-
-
-# Extract to ~/bin
-mkdir -p "$HOME"/bin
-sudo tar -xvzf lf-linux-amd64.tar.gz -C "$HOME"/bin/
-sudo chmod +x "$HOME"/bin/lf
-
-export_line="export PATH=\"$HOME/bin:$PATH\""
-echo "$export_line" >> ~/.bashrc 
-echo "Export $PATH:$HOME/bin command added to .bashrc"
-
-# Remove the archive
-rm lf-linux-amd64.tar.gz
-
-
-echo "Downloading the configs"
-
-git clone https://github.com/SumitKhatri712/dotfiles
-mkdir -p "$HOME"/.config/lf
-cp -r dotfiles/.config/lf "$HOME"/.config/
-chmod +x "$HOME"/.config/lf/preview
-
-
-echo "Installing dependencies"
+#!/bin/bash
 
 # Check distribution and perform package installation accordingly
 if [[ -f /etc/debian_version || -f /etc/lsb-release ]]; then
@@ -79,21 +47,3 @@ else
     echo "Unsupported distribution. Exiting..."
     exit 1
 fi
-
-
-echo "Setting Up Go-Mono nerd font"
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Go-Mono.zip > /dev/null 2>&1
-mkdir -p Go-Mono
-unzip Go-Mono.zip -d Go-Mono > /dev/null 2>&1
-sudo mv Go-Mono /usr/share/fonts/truetype/
-fc-cache -f -v > /dev/null 2>&1
-
-echo "Fetching and Compiling ctpv"
-git clone https://github.com/NikitaIvanovV/ctpv
-cd ctpv || exit
-make > /dev/null 2>&1
-sudo make install >/dev/null 2>&1
-cd ..
-rm -rf ctpv
-
-
