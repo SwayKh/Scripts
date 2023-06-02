e67e2e400bf31791b963ada5bc9a295aaf960f19
Sumit Khatri <khatrisumit712@gmail.com>
2023-06-02 17:46:43 +0530

    Updated lfInstall to export path

--- lfInstall.sh
+++ lfInstall.sh
@@ -6,12 +6,17 @@
 echo "Downloading Latest Release"
 
 # Download the archive
-wget https://github.com/horriblename/lf/releases/latest/download/lf-linux-amd64.tar.gz 
+wget https://github.com/horriblename/lf/releases/latest/download/lf-linux-amd64.tar.gz > /dev/null
+
 
 # Extract to ~/bin
 mkdir -p "$HOME"/bin
 sudo tar -xvzf lf-linux-amd64.tar.gz -C "$HOME"/bin/
 sudo chmod +x "$HOME"/bin/lf
+
+export_line="export PATH=\"$HOME/bin:$PATH\""
+echo "$export_line" >> ~/.bashrc 
+echo "Export $PATH:$HOME/bin command added to .bashrc"
 
 # Remove the archive
 rm lf-linux-amd64.tar.gz
@@ -41,16 +46,16 @@ else
 fi
 
 echo "Setting Up Go-Mono nerd font"
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Go-Mono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Go-Mono.zipz > /dev/null
 mkdir -p Go-Mono
-unzip Go-Mono.zip -d Go-Mono
+unzip Go-Mono.zip -d Go-Mono > /dev/null
 sudo mv Go-Mono /usr/share/fonts/truetype/
-fc-cache -f -v
+fc-cache -f -v > /dev/null
 
 echo "Fetching and Compiling ctpv"
 git clone https://github.com/NikitaIvanovV/ctpv
 cd ctpv || exit
-make
+make > /dev/null
 sudo make install
 cd ..
 rm -rf ctpv
