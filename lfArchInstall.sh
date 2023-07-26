826d4ccb7ab60a5df4d26e03a2f2778f570cddf5
Sumitkhatri712 <khatrisumit712@gmail.com>
2023-07-26 22:12:45 +0530

    update lfarchinstall.sh

--- lfArchInstall.sh
+++ lfArchInstall.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -S --needed lf
 
-read -n1 -r -p "Install my configs(sumitkhatri712)? [y,N]" doit 
+read -n1 -r -p "Install my configs and dependencies(sumitkhatri712)? [y,N]" doit 
 
 if [[ "$doit" == "y" || "$doit" == "Y" ]]; then
 	echo "Downloading the configs"
@@ -10,6 +10,7 @@ if [[ "$doit" == "y" || "$doit" == "Y" ]]; then
 	mkdir -p "$HOME"/.config/lf
 	cp -r dotfiles/.config/lf "$HOME"/.config/
 	chmod +x "$HOME"/.config/lf/preview
+  sudo pacman -S --needed chafa bat cat glow mdcat tar unrar unzip
 fi
 
 # Installing dependencies
