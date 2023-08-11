48ce436decbac5b7ae7f567477712dab25eda664
Sumitkhatri712 <khatrisumit712@gmail.com>
2023-08-11 22:22:26 +0530

    Update the lfInstall.sh script
    Updated the script to remove the complicated code
    and removed the Arch parts as I made a separate
    install script for that.
    This script should work on ubuntu and fedora
    Testing is pending

--- lfInstall.sh
+++ lfInstall.sh
@@ -1,47 +1,18 @@
 #!/bin/bash
 
+# FOR UBUNTU AND FEDORA USE THIS SCRIPT, FOR ARCH USE lfArchInstall.sh
 
 # Check if the script is running as root
 # if [[ $EUID -ne 0 ]]; then
-    # echo "This script must be run as root. Please run it with sudo or as root user."
-    # exit 1
+#     echo "This script must be run as root. Please run it with sudo or as root user."
+#     exit 1
 # fi
 
 # Set up LF file manager with one script, Installs all the packages and configs needed
 
-# No need for this code on Arch, LF is available in the Repo
-
-# # Download the latest release from GitHub
-# echo "Downloading Latest Release"
-# wget https://github.com/horriblename/lf/releases/latest/download/lf-linux-amd64.tar.gz
-# 
-# # Extract to ~/bin
-# mkdir -p "$HOME"/bin
-# tar -xvzf lf-linux-amd64.tar.gz -C "$HOME"/bin/
-# chmod +x "$HOME"/bin/lf
-# 
-# # Add to PATH
-# export_line="export PATH=\"$HOME/bin:$PATH\""
-# echo "$export_line" >> ~/.bashrc
-# echo "Export \"$PATH:$HOME/bin\" command added to .bashrc"
-# 
-# 
-# rm lf-linux-amd64.tar.gz
-
-
-
-# Check if the script is running as root
-if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root. Please run it with sudo or as root user."
-    exit 1
-fi
-
-# Set up LF file manager with one script, Installs all the packages and configs needed
 
 # Download the latest release from GitHub
 echo "Downloading Latest Release"
-
-# Download the archive
 wget https://github.com/horriblename/lf/releases/latest/download/lf-linux-amd64.tar.gz > /dev/null 2>&1
 
 # Extract to ~/bin
@@ -49,82 +20,36 @@ mkdir -p "$HOME"/bin
 tar -xvzf lf-linux-amd64.tar.gz -C "$HOME"/bin/
 chmod +x "$HOME"/bin/lf
 
+# Add to PATH
 export_line="export PATH=\"$HOME/bin:$PATH\""
 echo "$export_line" >> ~/.bashrc
-echo "Export \"$PATH:$HOME/bin\" command added to .bashrc"
+echo "Export PATH:~/bin command added to .bashrc"
 
-# Remove the archive
+
 rm lf-linux-amd64.tar.gz
 
-echo "Downloading the configs"
+read -n1 -r -p "Install my configs and dependencies(sumitkhatri712)? [y,N]" doit 
 
-git clone https://github.com/SumitKhatri712/dotfiles
-mkdir -p "$HOME"/.config/lf
-cp -r dotfiles/.config/lf "$HOME"/.config/
-chmod +x "$HOME"/.config/lf/preview
-
+if [[ "$doit" == "y" || "$doit" == "Y" ]]; then
+	echo "Downloading the configs"
+	git clone https://github.com/SumitKhatri712/dotfiles
+	mkdir -p "$HOME"/.config/lf
+	cp -r dotfiles/.config/lf "$HOME"/.config/
+	chmod +x "$HOME"/.config/lf/preview
+  sudo apt install bat cat tar unzip
+fi
 
 # Installing dependencies
 
-# Read the value of ID_LIKE from /etc/os-release
-if [[ -f /etc/os-release ]]; then
-    source /etc/os-release
-    distribution_like=($ID_LIKE)
-else
-    echo "Failed to detect distribution. Exiting..."
-    exit 1
-fi
-
-
 echo "Installing dependencies"
 
-# Check distribution similarity and perform package installation accordingly
-if [[ " ${distribution_like[@]} " =~ " arch " ]]; then
-    # Arch-based distributions
-    package_manager="pacman"
-    packages=("openssl" "file")
 
-    # Check if the packages are installed
-    if ! $package_manager -Qs "${packages[@]}" >/dev/null 2>&1; then
-        echo "Packages ${packages[*]} are not installed. Installing..."
-        # Install the packages
-        $package_manager -S --noconfirm "${packages[@]}"
-    else
-        echo "Packages ${packages[*]} are already installed."
-    fi
-
-elif [[ " ${distribution_like[@]} " =~ " debian " || " ${distribution_like[@]} " =~ " ubuntu " ]]; then
-    # Debian/Ubuntu and similar distributions
-    package_manager="apt"
-    packages=("libssl-dev" "libmagic-dev")
-
-    # Check if the packages are installed
-    if ! dpkg -s "${packages[@]}" >/dev/null 2>&1; then
-        echo "Packages ${packages[*]} are not installed. Installing..."
-        # Install the packages
-        $package_manager install -y "${packages[@]}"
-    else
-        echo "Packages ${packages[*]} are already installed."
-    fi
-
-elif [[ " ${distribution_like[@]} " =~ " fedora " ]]; then
-    # Fedora and similar distributions
-    package_manager="dnf"
-    packages=("openssl-devel" "file-devel")
-
-    # Check if the packages are installed
-    if ! rpm -q "${packages[@]}" >/dev/null 2>&1; then
-        echo "Packages ${packages[*]} are not installed. Installing..."
-        # Install the packages
-        $package_manager install -y "${packages[@]}"
-    else
-        echo "Packages ${packages[*]} are already installed."
-    fi
-
-else
-    echo "Unsupported distribution. Exiting..."
-    exit 1
+if command -v apt &>/dev/null; then
+    sudo apt install -y libmagic-dev libssl-dev
+elif command -v dnf &>/dev/null; then
+    sudo dnf install -y openssl-devel file-devel
 fi
+
 
 echo "Setting Up Go-Mono nerd font"
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Go-Mono.zip > /dev/null 2>&1
@@ -140,3 +65,6 @@ make > /dev/null 2>&1
 make install >/dev/null 2>&1
 cd ..
 rm -rf ctpv
+
+echo " "
+echo "Please set your Terminal Font to a nerd-font before proceeding"
