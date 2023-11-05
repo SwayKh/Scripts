e1cdf57aed96cbed85e74b03fe584ae92f702071
SwayKh <khatrisumit712@gmail.com>
2023-11-05 17:01:59 +0530

    Trying out rofi -dmenu for powermenu widget

--- rofi/sessionMenu.sh
+++ rofi/sessionMenu.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-uptime=$(uptime -p | sed -e 's/up //g')
+# uptime=$(uptime -p | sed -e 's/up //g')
 
 # Rofi dmenu mode, -i make search case-insensitive, -l is the number of line
-THEME="/home/sumit/.config/rofi/scripts/powerconfig.rasi"
+THEME="/home/sumit/scripts/rofi/powerconfig.rasi"
 rofi_command() {
   rofi -dmenu \
-        -theme /home/sumit/.config/rofi/scripts/powerconfig.rasi \
-        -i \ 
-        -l 5 
+        -theme "$THEME" \
+        -i \
+
 }
+
 shutdown="󰐥 | Shutdown" 
 reboot=" | Restart"
 lock=" | Lock"
@@ -19,6 +20,6 @@ logout="󰍂 | Logout"
 options="$shutdown\n$reboot\n$logout\n$suspend\n$lock"
 
 chosen="$(echo -e "$options" | rofi_command)"
-echo $chosen
+echo "$chosen"
 
 
