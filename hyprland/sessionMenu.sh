b1d0e69ec8a08a256300a6d32650ccc9cda54d9d
SwayKh <khatrisumit712@gmail.com>
2023-12-08 20:07:07 +0530

    Finally make the theme for powermenu for rofi, remove all previous tries

--- hyprland/sessionMenu.sh
+++ hyprland/sessionMenu.sh
@@ -1,49 +1,48 @@
 #!/usr/bin/env bash
 
-uptime=$(uptime -p | sed -e 's/up //g')
-
 # Rofi dmenu mode, -i make search case-insensitive, -l is the number of line
-rofi_command="rofi -dmenu -i -l 5"
+rofi_command() {
+  rofi -dmenu -i -theme-str '@import "~/scripts/rofi/power.rasi"'
+}
 
-shutdown="󰐥 | Shutdown" 
+shutdown="󰐥 | Shutdown"
 reboot=" | Restart"
 lock=" | Lock"
 suspend="󰤄 | Suspend"
-logout="󰍂 | Logout" 
+logout="󰍂 | Logout"
 
 options="$shutdown\n$reboot\n$logout\n$suspend\n$lock"
 
-chosen="$(echo -e "$options" | $rofi_command)"
-echo $chosen
+chosen="$(echo -e "$options" | rofi_command)"
+echo "$chosen"
 case $chosen in
-    $shutdown)
-			systemctl poweroff
-        ;;
-    $reboot)
-			systemctl reboot
-        ;;
-    $lock)
-      # Install swaylock-effects for better configuration
-			swaylock -l
-        ;;
-    $suspend)
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-        ;;
-    $logout)
-      # For Hyprland, Use Command for your WM/DE
-      hyprctl dispatch exit
+"$shutdown")
+  systemctl poweroff
+  ;;
+"$reboot")
+  systemctl reboot
+  ;;
+"$lock")
+  # Install swaylock-effects for better configuration
+  swaylock -l
+  ;;
+"$suspend")
+  mpc -q pause
+  amixer set Master mute
+  systemctl suspend
+  ;;
+"$logout")
+  # For Hyprland, Use Command for your WM/DE
+  hyprctl dispatch exit
 
-			# if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
-			# 	openbox --exit
-			# elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-			# 	bspc quit
-			# elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-			# 	i3-msg exit
-			# elif [[ "$DESKTOP_SESSION" == "xfce" ]]; then
-			# 	killall xfce4-session
-			# fi
-        ;;
+  # if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+  # 	openbox --exit
+  # elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+  # 	bspc quit
+  # elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+  # 	i3-msg exit
+  # elif [[ "$DESKTOP_SESSION" == "xfce" ]]; then
+  # 	killall xfce4-session
+  # fi
+  ;;
 esac
-
