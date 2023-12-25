d7194e0a68b3e127efff397a27465117ae920e7d
SwayKh <khatrisumit712@gmail.com>
2023-12-25 22:57:48 +0530

    Move screenshots scripts out of hyprland directory, remove double randomWallBg

--- hyprland/randomWallBg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wall=$(command ls ~/wallpapers/Images/ | shuf -n 1)
-path="/home/sumit/wallpapers/Images/$wall"
-killall swaybg
-cp "$path" ~/.config/wall.jpg && swaybg -m fill -i "$path" &
