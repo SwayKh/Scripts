59ccef2f80a7a2c4dcbdc7d8bc160c4921f93a56
SwayKh <khatrisumit712@gmail.com>
2024-03-15 23:49:26 +0530

    Power.rasi now uses the main color.rasi theme, Hyprland updated setup script

--- hyprland/setup.sh
+++ hyprland/setup.sh
@@ -2,10 +2,12 @@
 
 hyprctl dispatch exec [workspace 1 silent] firefox
 sleep 1
-hyprctl dispatch exec [workspace 2 silent] "wezterm -e nvim"
+hyprctl dispatch exec [workspace 2 silent] "foot -e nvim ~/dotfiles/"
 sleep 1
-hyprctl dispatch exec [workspace 3 silent] "wezterm -e lf"
+hyprctl dispatch exec [workspace 3 silent] foot
 sleep 1
-hyprctl dispatch exec [workspace 4 silent] wezterm
+hyprctl dispatch exec [workspace 4 silent] foot
 sleep 1
-hyprctl dispatch exec [workspace 9 silent] obsidian
+hyprctl dispatch exec [workspace 5 silent] "foot -e lf"
+sleep 1
+hyprctl dispatch exec [workspace 6 silent] retroarch
