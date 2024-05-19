5a4b9e0eb57a3c676d4f06ec79f45782a73d040d
SwayKh <khatrisumit712@gmail.com>
2024-05-19 13:34:01 +0530

    Turn all zshrc functions to script executables, And move some functions to ~/scripts

---
+++ fpreview.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+#
+# Preview script for fzf used in vairous scripts
+
+filetype=$(cliphist decode "$1" | file --mime-type -)
+
+if [[ $filetype =~ "image" ]]; then
+  cliphist decode "$1" |
+    chafa -f sixels \
+      -s "${FZF_PREVIEW_COLUMNS}"x"${FZF_PREVIEW_LINES}" -
+else
+  cliphist decode "$1" |
+    bat --color=always \
+      --style=numbers,grid \
+      --wrap=auto \
+      --theme=gruvbox-dark \
+      --terminal-width "${FZF_PREVIEW_COLUMNS}"
+fi
---
+++ function.sh
@@ -0,0 +1,43 @@
+lfcd() {
+  tmp="$(mktemp)"
+  lf -last-dir-path="$tmp" "$@"
+  if [ -f "$tmp" ]; then
+    dir="$(cat "$tmp")"
+    rm -f "$tmp"
+    [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir" || exit
+  fi
+}
+
+pacPreviewAll() {
+  pacman -Slq | fzf --preview 'pacman -Si {}' --layout=reverse
+}
+
+pacPreviewInstalled() {
+  pacman -Qeq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'
+}
+
+pacPreviewInstalledAll() {
+  pacman -Qq | fzf --preview 'pacman -Qil {}' --layout=reverse --bind 'enter:execute(pacman -Qil {} | less)'
+}
+
+rename_directories() {
+  for dir in "$1"/*; do
+    if [ -d "$dir" ]; then
+      file=$(basename "$dir"/*)
+      dir_path=$(dirname "$dir")
+      mv "$dir" "$dir_path/$file"
+      echo "Renamed '$dir' to '$dir_path/$file'"
+    fi
+  done
+}
+
+# This needs to be defined in root user.
+#  recovery-pacman() {
+#     pacman "$@"  \
+#     --log /dev/null   \
+#     --noscriptlet     \
+#     --dbonly          \
+#     --overwrite "*"   \
+#     --nodeps          \
+#     --needed
+# }
