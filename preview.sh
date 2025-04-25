a8741c6ffba37f392fa31166336031b19d3dac48
SwayKh <khatrisumit712@gmail.com>
2025-04-25 23:42:13 +0530

    Cleanup fzf preview script, also remove the "rsync Roms" part of update-roms
    update-roms script now just updated the obsidian note

--- preview.sh
+++ preview.sh
@@ -29,7 +29,10 @@ if [ -d "$1" ]; then
   exit 0
 fi
 
-CACHE="$HOME/.cache/lf/thumbnail.$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | awk '{print $1}'))"
+if [ ! -d "$HOME/.cache/tmp" ]; then
+  mkdir -p "$HOME/.cache/tmp"
+fi
+CACHE="$HOME/.cache/tmp/thumbnail.$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | awk '{print $1}'))"
 
 case "$(printf "%s\n" "$(readlink -f "$1")" | awk '{print tolower($0)}')" in
 *.tgz | *.tar.gz) tar tzf "$1" ;;
@@ -91,52 +94,3 @@ case "$(printf "%s\n" "$(readlink -f "$1")" | awk '{print tolower($0)}')" in
   batorcat "$1"
   ;;
 esac
-
-# # Cleanup function
-# cleanup() {
-#   [[ -n "$thumb_cache" ]] && rm -f "$thumb_cache"
-# }
-# trap cleanup EXIT
-#
-# # MIME type of the file
-# mime=$(file --mime-type -b "$1" 2>/dev/null)
-#
-# # Handle different MIME types
-# case "$mime" in
-# text/*)
-#   # Use bat for text files
-#   bat --color=always --style=numbers "$1" 2>/dev/null
-#   ;;
-# inode/directory)
-#   tree "$1" 2>/dev/null
-#   ;;
-# image/jpeg | image/png | image/svg+xml)
-#   # Use magick to display images in the terminal using sixel
-#   # magick "$1" -geometry 800x480 sixel:- 2>/dev/null
-#   # or use chafa
-#   chafa "$1" -f sixels -s "${FZF_PREVIEW_COLUMNS}"x"${FZF_PREVIEW_LINES}"
-#   ;;
-# application/pdf)
-#   # Use pdftotext for PDF files
-#   pdftotext "$1" - | head -n 1000 2>/dev/null
-#   ;;
-# application/epub+zip)
-#   # Use epub2txt for EPUB files
-#   epub2txt "$1" | head -n 1000 2>/dev/null
-#   ;;
-# video/*)
-#   # Generate a thumbnail for video files
-#   thumb_cache=$(mktemp "${TMPDIR:-/tmp}/thumb_cache.XXXXX.png")
-#   ffmpegthumbnailer -i "$1" -o "$thumb_cache" -s 0 2>/dev/null
-#   # Display the thumbnail
-#   img2sixel <"$thumb_cache" 2>/dev/null
-#   ;;
-# application/x-tar | application/gzip | application/x-compressed-tar | application/x-bzip2 | application/x-xz | application/zip | application/x-7z-compressed | application/x-rar-compressed)
-#   ~/.config/lf/pistol-static-linux-x86_64 "$1" 2>/dev/null
-#   ;;
-# *)
-#   # Default: show file details
-#   echo "File type: $mime"
-#   echo "No preview available for this file type."
-#   ;;
-# esac
