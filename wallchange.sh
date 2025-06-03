d7a1f10f0399b07a928f5d8a843b662dd77c062c
SwayKh <khatrisumit712@gmail.com>
2025-06-03 15:19:15 +0530

    Update wallpaper scripts pywal flags

--- wallchange.sh
+++ wallchange.sh
@@ -6,11 +6,9 @@
 # Load Pywal16 colors to apply to borders
 source "$HOME/.cache/wal/colors.sh"
 
-sleep 0.5
 # Apply colors to RiverWM border
 riverctl border-color-focused "0x${color6}"
 riverctl border-color-unfocused "0x${color1}"
 
-sleep 0.5
 # Restart Dunst with new color
 "$HOME/.config/dunst/launch-dunst.sh"
