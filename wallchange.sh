69db6889c985a453b77346b097aecf9d1c228d57
SwayKh <khatrisumit712@gmail.com>
2025-06-06 22:44:36 +0530

    Change shebang in wallchange.sh and volume script to match other scripts

--- wallchange.sh
+++ wallchange.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /usr/bin/env bash
 
 # Use this as script to run after Pywal16 runs
 
@@ -9,8 +9,8 @@
 source "$HOME/.cache/wal/colors.sh"
 
 # Apply colors to RiverWM border
-riverctl border-color-focused "0x${color6}"
-riverctl border-color-unfocused "0x${color1}"
+# riverctl border-color-focused "0x${color6}"
+# riverctl border-color-unfocused "0x${color1}"
 
 # Restart Dunst with new color
 "$HOME/.config/dunst/launch-dunst.sh"
