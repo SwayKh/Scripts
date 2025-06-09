492954ccc3579ce8f3360758714cb6076513542c
SwayKh <khatrisumit712@gmail.com>
2025-06-09 17:31:19 +0530

    Update pywal commands in scripts, to run the launch-dunst script.

--- wallchange.sh
+++ wallchange.sh
@@ -6,12 +6,12 @@
 # "$HOME/scripts/random-swwwbg"
 
 # Load Pywal16 colors to apply to borders
-source "$HOME/.cache/wal/colors.sh"
+# source "$HOME/.cache/wal/colors.sh"
 
 # Apply colors to RiverWM border
 # riverctl border-color-focused "0x${color6}"
 # riverctl border-color-unfocused "0x${color1}"
 
 # Restart Dunst with new color
-sleep 1
-"$HOME/.config/dunst/launch-dunst.sh"
+# sleep 1
+# "$HOME/.config/dunst/launch-dunst.sh"
